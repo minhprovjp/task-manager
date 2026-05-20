35a0b32b1a87db698bbf0706993df9cc446800ba
minhprovjp <anhndmfs90955@fpt.edu.vn>
2026-05-21 01:15:32 +0700

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-define('LOCALHOST', 'localhost');
+define('LOCALHOST', '0.0.0.0');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
 define('DB_NAME', 'task_manager');
 ini_set('mysqli.default_socket', '/tmp/mysql-run/mysql.sock');
-define('SITEURL', 'http://localhost:8080/');
+define('SITEURL', 'http://0.0.0.0:8080/');

a07978c493ca060dff5fa55c0bf44388ab71e4ce
minhprovjp <anhndmfs90955@fpt.edu.vn>
2026-06-02 03:12:05 +0700

    update

--- login.php
+++ login.php
@@ -68,7 +68,7 @@
                         <td colspan="2">
                             <input class="btn-primary btn-lg" type="submit" name="submit" value="Login" />
                             &nbsp;&nbsp;
-                            <a href="<?php echo SITEURL; ?>signup.php" class="btn-primary btn-lg" style="margin-top: 10px; padding: 10px 20px; text-decoration: none; display: inline-block;">Sign Up</a>
+                            <a href="<?php echo SITEURL; ?>signup.php" class="btn-primary btn-lg" style="margin-top: 10px; padding: 10px 20px;">Sign Up</a>
                         </td>
                     </tr>
                 </table>

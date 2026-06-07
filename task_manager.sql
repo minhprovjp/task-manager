e4b4885fa27251311e6e3436c352cf61dec1f1b9
minhprovjp <anhndmfs90955@fpt.edu.vn>
2026-06-07 17:31:05 +0700

    modified:   task_manager.sql

--- task_manager.sql
+++ task_manager.sql
@@ -48,14 +48,14 @@ CREATE TABLE `tbl_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 INSERT INTO `tbl_users` (`user_id`, `username`, `password`, `email`, `role`, `token`) VALUES
-(1, 'admin', 'sup3r_s3cr3t_p4ss', 'admin@taskmanager.local', 'admin', '4dm1n_T0k3n_h3re'),
+(1, 'admin', 'sup3r_s3cr3t_p4ss', 'admin@taskmanager.local', 'admin', 'un10n_1s_p0w3rful'),
 (2, 'staff', 'staff123', 'staff@taskmanager.local', 'user', '3rr0r_b4s3d_m4st3r'),
 (4, 'intern', 'intern123', 'intern@taskmanager.local', 'user', 'b00l34n_bl1nd_pr0'),
 (5, 'contractor', 'contr123', 'contractor@taskmanager.local', 'user', 't1m3_1s_1lus10n');
 
 
 INSERT INTO `tbl_users` (`user_id`, `username`, `password`, `email`, `role`, `token`) VALUES
-(3, 'manager', 'm4n4g3r_p4ss', 'manager@taskmanager.local', 'admin', 'un10n_1s_p0w3rful');
+(3, 'manager', 'm4n4g3r_p4ss', 'manager@taskmanager.local', 'admin', 'M@n4g3r_T0k3n_h3re');
 
 CREATE TABLE `tbl_feedback` (
   `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT,
